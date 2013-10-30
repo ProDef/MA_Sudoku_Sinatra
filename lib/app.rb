7c6cd832395827ee3d900c71c19f06c3fee07c77
Hamilton Chapman <hamchapman@gmail.com>
2013-10-30 19:37:46 +0000

    Seems to all be working

--- lib/app.rb
+++ lib/app.rb
@@ -14,12 +14,12 @@ def random_sudoku
 end
 
 def remove_one_value_from sudoku
-	sudoku[rand(sudoku.count)] = ' '
+	sudoku[rand(sudoku.count)] = 0
 	sudoku
 end
 
 def count_blanks_in sudoku
-	sudoku.count(' ')
+	sudoku.count(0)
 end
 
 def insert_number_of_blanks_in sudoku, number
@@ -30,18 +30,96 @@ def insert_number_of_blanks_in sudoku, number
 end
 
 def puzzle(sudoku)
-	insert_number_of_blanks_in sudoku, 40
-  sudoku 
+	sudoku_copy = sudoku.dup
+	insert_number_of_blanks_in sudoku_copy, 40
+end
+
+def prepare_to_check_solution
+  @check_solution = session[:check_solution]
+  session[:check_solution] = nil
+end
+
+def generate_new_puzzle_if_necessary
+  return if session[:current_solution]
+  sudoku = random_sudoku
+  session[:solution] = sudoku
+  session[:puzzle] = puzzle(sudoku)
+  session[:current_solution] = session[:puzzle]    
+end
+
+def box_order_to_row_order(cells)
+  boxes = cells.each_slice(9).to_a
+  (0..8).to_a.inject([]) { |memo, i|
+    first_box_index = i / 3 * 3
+    three_boxes = boxes[first_box_index, 3]
+    three_rows_of_three = three_boxes.map do |box| 
+      row_number_in_a_box = i % 3
+      first_cell_in_the_row_index = row_number_in_a_box * 3
+      box[first_cell_in_the_row_index, 3]
+    end
+    memo += three_rows_of_three.flatten
+  }
+end
+
+helpers do
+  
+  def colour_class(solution_to_check, puzzle_value, current_solution_value, solution_value)
+    must_be_guessed = puzzle_value.to_i ==  0
+    tried_to_guess = current_solution_value.to_i != 0
+    guessed_incorrectly = current_solution_value != solution_value
+    # puts puzzle_value.to_i
+    # puts tried_to_guess
+    # puts solution_value
+    # puts current_solution_value
+
+    if solution_to_check && must_be_guessed && tried_to_guess && guessed_incorrectly
+      'incorrect'
+    elsif !must_be_guessed
+      'value-provided'
+    end
+  end
+  
+  def cell_value(value)
+    value.to_i == 0 ? ' ' : value
+  end
+
 end
 
 get '/' do
-  sudoku = random_sudoku
-  session[:solution] = sudoku
-  @current_solution = puzzle(sudoku)
+  # sudoku = random_sudoku
+  # session[:solution] = sudoku
+  prepare_to_check_solution
+  generate_new_puzzle_if_necessary
+  @current_solution = session[:current_solution] || session[:puzzle]
+  @puzzle = session[:puzzle]
+  @solution = session[:solution]
   erb :index
 end
 
+# get '/solution' do
+#   @current_solution = session[:solution]
+#   @puzzle = @current_solution
+#   @solution = session[:solution]
+#   erb :index
+# end
+
 get '/solution' do
   @current_solution = session[:solution]
+  @puzzle = session[:puzzle]
+  @solution = session[:solution]
   erb :index
 end
+
+post '/' do
+  # the cells in HTML are ordered box by box (first box1, then box2, etc),
+  # so the form data (params['cell']) is sent using this order
+  # However, our code expects it to be row by row, so we need to transform it.
+  # puts session[:current_solution]
+  cells = box_order_to_row_order(params["cell"])
+  # puts cells.inspect
+  session[:current_solution] = cells.map{|value| value.to_i }.join
+  session[:check_solution] = true
+  redirect to("/")
+end
+
+
