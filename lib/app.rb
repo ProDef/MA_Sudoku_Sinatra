fa913ab65853d927d2bfb48fa3ea6179de37d4cd
Hamilton Chapman <hamchapman@gmail.com>
2013-10-31 10:33:14 +0000

    Started to style CSS

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require_relative 'sudoku'
 require_relative 'cell'
+require_relative 'helpers'
 
 enable :sessions
 
@@ -59,30 +60,6 @@ def box_order_to_row_order(cells)
     end
     memo += three_rows_of_three.flatten
   }
-end
-
-helpers do
-  
-  def colour_class(solution_to_check, puzzle_value, current_solution_value, solution_value)
-    must_be_guessed = puzzle_value.to_i ==  0
-    tried_to_guess = current_solution_value.to_i != 0
-    guessed_incorrectly = current_solution_value != solution_value
-    # puts puzzle_value.to_i
-    # puts tried_to_guess
-    # puts solution_value
-    # puts current_solution_value
-
-    if solution_to_check && must_be_guessed && tried_to_guess && guessed_incorrectly
-      'incorrect'
-    elsif !must_be_guessed
-      'value-provided'
-    end
-  end
-  
-  def cell_value(value)
-    value.to_i == 0 ? ' ' : value
-  end
-
 end
 
 get '/' do
